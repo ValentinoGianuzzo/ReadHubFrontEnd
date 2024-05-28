0c3d1ece9d29ca6fbe81d6fa4e4767abe2348a08
ValentinoGianuzzo <valentino.gianuzzo@ing.austral.edu.ar>
2024-05-28 13:01:46 -0300

    book display

--- src/Rutas/book/Detail.tsx
+++ src/Rutas/book/Detail.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import axios from 'axios';
+import StarRatings from 'react-star-ratings';
 
 interface Book {
     volumeInfo: {
         title: string;
+        authors: string[];
         description: string;
+        imageLinks: {
+            thumbnail: string;
+        };
+        averageRating: number;
     };
 }
 
 export default function Detail() {
-    const { bookId } = useParams();
+    const {bookId} = useParams();
     const [book, setBook] = useState<Book | null>(null);
+    const [rating, setRating] = useState(0);
 
     useEffect(() => {
         const fetchBook = async () => {
@@ -26,14 +33,44 @@ export default function Detail() {
         fetchBook();
     }, [bookId]);
 
+    const changeRating = (newRating: number) => {
+        setRating(newRating);
+    };
+
     return (
         <div>
             {book ? (
                 <>
-                    <h1>{book.volumeInfo.title}</h1>
-                    <h2>book.
-                    </h2>
-                    <p>{book.volumeInfo.description}</p>
+                    <div style={{display: 'flex', flexDirection: 'row'}}>
+                        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}
+                             style={{marginTop: '60px' ,marginRight: '40px',marginLeft: '20px',maxHeight: '300px',maxWidth:'300px',minHeight: '300px',minWidth:'300px'}}/>
+                        <div>
+                            <h1>{book.volumeInfo.title}</h1>
+                            <h2>{book.volumeInfo.authors.join(', ')}</h2>
+                            <div className="bookDescription">
+                                <p>
+                                    {book.volumeInfo.description}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Rating: {book.volumeInfo.averageRating}</p>
+                        <StarRatings
+                            rating={rating}
+                            starRatedColor="blue"
+                            changeRating={changeRating}
+                            numberOfStars={5}
+                            name='rating'
+                        />
+                    </div>
+                    <div>
+                        <h2>Comments</h2>
+                        <form>
+                            <textarea placeholder="Write a comment..."></textarea>
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
                 </>
             ) : (
                 <p>Loading...</p>
